ba5c7ad36a96da8bc61f299a789caa1534b0799d
James Schurig <jnschurig@gmail.com>
2024-09-18 08:46:25 -0600

    Adding a shell.nix for doing actual development

--- shell.nix
+++ shell.nix
@@ -1,3 +1,5 @@
+# Consider using mach-nix to generate these in the future: https://github.com/DavHau/mach-nix
+# An example: https://discourse.nixos.org/t/how-to-convert-a-python-requirements-txt-file-into-a-nix-script/12426/2
 let
   pkgs = import <nixpkgs> {};
 in pkgs.mkShell {
